5d9d439a0ebd10ec497ecfd8294733fcffc94443
James Chiu <jachiu555@gmail.com>
2020-05-07 02:55:21 +0000

    Defined a JS class named App. Defined the handleGetGradesError, handleGetGradesSuccess, constructor, getGrades, and start methods. Used handleGetGradesSuccess and handleGetGradesError callbacks in the success and error portion of ajax config object. Properly instantiated App class in main.js and called the start method in app.js successfully. Can see data in the Network tab and received data via Console tab.

---
+++ components/app.js
@@ -0,0 +1,32 @@
+class App {
+  constructor() {
+    this.handleGetGradesError = this.handleGetGradesError.bind(this);
+    this.handleGetGradesSuccess = this.handleGetGradesSuccess.bind(this);
+  };
+
+  handleGetGradesError(error) {
+    console.error(error);
+  };
+
+  handleGetGradesSuccess(grades) {
+    console.log(grades);
+  };
+
+  getGrades() {
+    $.ajax({
+      type: "GET",
+      url: "https://sgt.lfzprototypes.com/api/grades",
+      timeout: 2000,
+      data: "none",
+      headers: {
+        "X-Access-Token": "Npew7LVS"
+      },
+      success: this.handleGetGradesSuccess,
+      error: this.handleGetGradesError
+    });
+  };
+
+  start() {
+    this.getGrades();
+  }
+};
