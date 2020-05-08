4b4004d5a69d82921051b1580a057f7d0957409e
James Chiu <jachiu555@gmail.com>
2020-05-08 04:49:09 +0000

    Looped through grades parameter and for each item, created a new <tr> element with 3 separated <td> elements. Appended name, course, and grade inside grades array to the <td> elements. Appended them to <tr> and appended <tr> to <tbody>. Table properly displays when refreshed.

--- components/grade-table.js
+++ components/grade-table.js
@@ -8,5 +8,22 @@ class GradeTable {
     let tBody = this.tableElement.querySelector("tbody");
 
     tBody.innerHTML = "";
+
+    console.log(grades);
+
+    for (let i = 0; i < grades.length; i++) {
+      let tr = document.createElement("tr");
+      let td = document.createElement("td");
+      let td1 = document.createElement("td");
+      let td2 = document.createElement("td");
+
+      let name = td.append(grades[i].name);
+      let course = td1.append(grades[i].course);
+      let grade = td2.append(grades[i].grade);
+
+      tr.append(td, td1, td2);
+
+      tBody.appendChild(tr);
+    };
   };
 };
