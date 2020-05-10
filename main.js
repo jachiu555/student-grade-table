11842f15f738c74bd11d19f7dc2e7b612670dfde
James Chiu <jachiu555@gmail.com>
2020-05-10 07:16:07 +0000

    Queried document for header element. Instantiated PageHeader and passed header element in as an argument.

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 let tableElement = document.getElementsByClassName("table")[0];
 let gradeTable = new GradeTable(tableElement);
+let pageHeaderElement = document.getElementsByClassName("header")[0];
+let pageHeader = new PageHeader(pageHeaderElement);
 
-let app = new App(gradeTable);
+let app = new App(gradeTable, pageHeader);
 
 app.start();
