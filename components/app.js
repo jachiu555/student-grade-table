e2b6057a5253ff8ad4ec167d1911be9103286ef3
James Chiu <jachiu555@gmail.com>
2020-05-13 04:18:53 +0000

    Binded createGrade, handleCreateGradeError, and handleCreateGradeSuccess in App's constructor.

--- components/app.js
+++ components/app.js
@@ -6,6 +6,9 @@ class App {
 
     this.handleGetGradesError = this.handleGetGradesError.bind(this);
     this.handleGetGradesSuccess = this.handleGetGradesSuccess.bind(this);
+    this.createGrade = this.createGrade.bind(this);
+    this.handleCreateGradeError = this.handleCreateGradeError.bind(this);
+    this.handleCreateGradeSuccess = this.handleCreateGradeSuccess.bind(this);
   };
 
   handleGetGradesError(error) {
