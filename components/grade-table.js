1cc55f7e8137f62af5f6b5dde02c6c94e9522c7a
James Chiu <jachiu555@gmail.com>
2020-05-15 01:32:57 +0000

    Modified GradeTable's update method to use this.renderGradeRow to create each row in a loop. Passed in each grade object along with this.deleteGrade as a callback.

--- components/grade-table.js
+++ components/grade-table.js
@@ -9,18 +9,20 @@ class GradeTable {
     tBody.innerHTML = "";
 
     for (let i = 0; i < grades.length; i++) {
-      let tr = document.createElement("tr");
-      let td = document.createElement("td");
-      let td1 = document.createElement("td");
-      let td2 = document.createElement("td");
+      // let tr = document.createElement("tr");
+      // let td = document.createElement("td");
+      // let td1 = document.createElement("td");
+      // let td2 = document.createElement("td");
 
-      let name = td.append(grades[i].name);
-      let course = td1.append(grades[i].course);
-      let grade = td2.append(grades[i].grade);
+      // let name = td.append(grades[i].name);
+      // let course = td1.append(grades[i].course);
+      // let grade = td2.append(grades[i].grade);
 
-      tr.append(td, td1, td2);
+      // tr.append(td, td1, td2);
 
-      tBody.appendChild(tr);
+      // tBody.appendChild(tr);
+
+      tBody.append(this.renderGradeRow(grades[i], this.deleteGrade));
     };
   };
 
